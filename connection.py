c856680bf7f9d528b082d6bea6be00b6c90610ec
Pawan-Koder <1213056@genstudents.org>
2026-05-18 11:00:21 +0100

    Created functions for database connection, table creation, and table data extraction.

--- connection.py
+++ connection.py
@@ -11,44 +11,118 @@ database_name = os.environ.get("POSTGRES_DB")
 user_name = os.environ.get("POSTGRES_USER")
 user_password = os.environ.get("POSTGRES_PASSWORD")
 
-try:
+def create_database_tables():
+    try:
 
-    ### SETUP THE DATABASE CONNECTION
-    print('Opening connection...')
-    conn_string = f'host={host_name} dbname={database_name} user={user_name} password={user_password}'
-    # Establish a database connection
-    with psycopg2.connect(conn_string) as connection:
+        ### SETUP THE DATABASE CONNECTION
+        print('Opening connection...')
+        conn_string = f'host={host_name} dbname={database_name} user={user_name} password={user_password}'
+        # Establish a database connection
+        with psycopg2.connect(conn_string) as connection:
 
-        print('Opening cursor...')
-        cursor = connection.cursor()
-        cursor.execute("DROP TABLE IF EXISTS products;")
+            print('Opening cursor...')
+            cursor = connection.cursor()
 
-        print('Creating table...')
-        create_table_sql = create_table()
-        cursor.execute(create_table_sql)
-    
+    # ============================================
+    #                CREATE TABLES
+
+            print('Creating tables...')
+
+            cursor.execute("DROP TABLE IF EXISTS products;")
+            create_table_sql = create_product_table()
+            cursor.execute(create_table_sql)
+            print('Product table created successfully.')
+
+            cursor.execute("DROP TABLE IF EXISTS couriers;")
+            create_table_sql = create_courier_table()
+            cursor.execute(create_table_sql)
+            print('Courier table created successfully.')
+
+
+    # ============================================
+    #                FILL TABLES FROM CSV
+
+            print('Filling tables from CSV files...')
+
+            # EXAMPLE
+            new_product(cursor, 'Pizza', 9.99)
+
+
+            # fill_products_table()
+
+            # fill_couriers_table()
+
+            # fill_orders_table()
+
+
+    # ============================================
+    #                 LOAD TO DATABASE
+
+            print('\n\nCommiting. . .')
+            connection.commit()
+
+            print('Displaying all records. . .')
+            cursor.execute("SELECT * FROM products;")
+            records = cursor.fetchall()
+            for row in records:
+                print(row)
+
+            print('\nClosing cursor. . .')
+            cursor.close()
+            print('All done!')
+            # The connection will automatically close here
+
+    except Exception as ex:
+        print('Failed to:', ex)
+
+
+
+
+
+# =================================================================================
+# =================================================================================
         
+def extract_from_database():
+    try:
 
-# ============================================
-#                 LOAD
-
-        print('Commiting. . .')
-        connection.commit()
-
-        print('Displaying all records. . .')
-        cursor.execute("SELECT * FROM products;")
-        records = cursor.fetchall()
-        for row in records:
-            print(row)
-
-        print('Closing cursor. . .')
-        # Closes the cursor so will be unusable from this point
-        cursor.close()
-
-        # The connection will automatically close here
-except Exception as ex:
-    print('Failed to:', ex)
-
-# Leave this line here!
-print('All done!')
+    # ============================================
+    #            SETUP THE DATABASE CONNECTION
         
+        print('Opening connection...')
+        conn_string = f'host={host_name} dbname={database_name} user={user_name} password={user_password}'
+        # Establish a database connection
+        with psycopg2.connect(conn_string) as connection:
+
+            print('Opening cursor...')
+            cursor = connection.cursor()
+
+    # ============================================
+    #                EXTRACT DATA
+
+            print('Extracting data from database...')
+
+            print('Displaying all records. . .')
+            cursor.execute("SELECT * FROM products;")
+            records = cursor.fetchall()
+            for row in records:
+                print(row)
+            
+            # EXTRACT DATA FROM COURIER TABLE
+
+            # EXTRACT DATA FROM ORDERS TABLE
+
+    # ============================================
+    #                 CLOSE CONNECTION
+
+            print('\nClosing cursor. . .')
+            # Closes the cursor so will be unusable from this point
+            cursor.close()
+            print('All done!')
+
+            # The connection will automatically close here
+    except Exception as ex:
+        print('Failed to:', ex)
+
+    print("Connection closed.")
+
+
