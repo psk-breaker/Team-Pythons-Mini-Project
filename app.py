bf2c602df3bf4c58872963c8f07a761093702d40
Pawan-Koder <1213056@genstudents.org>
2026-05-20 13:54:43 +0100

    Clean project without tasks.txt and save.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from products import *
 from orders import *
 from couriers import *
 from display import *
-from save import *
 from connection import *
 
 # ================================================================================
@@ -24,9 +23,6 @@ while is_app_running == True:
 
     if choice == 0: 
         # load_into_database()
-        # save_products_to_csv(Products)
-        # save_orders_to_csv(orders)
-        # save_couriers_to_csv(couriers)
 
         print("Exiting app") 
         is_app_running = False # This will finally fail the while loop condition
